733b103bb4b05751c15cbd529cf68ac6137a9454
Greger Martinussen <greger.martinussen@no.experis.com>
2024-08-22 12:49:13 +0200

    update

--- Function1.cs
+++ Function1.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -21,11 +25,54 @@ namespace FunctionApp1
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            var dataSet = new MockData
+            {
+                CaseNumber = "NPE-2024-00240",
+                Name = "Ola Nordmann",
+                TreatmentCenter = "Oslo Universitetssykehus",
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        QuestionText = "Hva er ditt navn?",
+                        Answer = "Ola Nordmann"
+                    },
+                    new Question
+                    {
+                        QuestionText = "Hva er din alder?",
+                        Answer = "45"
+                    },
+                    new Question
+                    {
+                        QuestionText = "Hva er din adresse?",
+                        Answer = "Osloveien 1, 0010 Oslo"
+                    }
+                }
+            };
 
+            response.Body = new MemoryStream(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(dataSet)));
             return response;
         }
+    }
+    public class FormData
+    {
+        [JsonPropertyName("formUserName")]
+        public string ApplicantName { get; set; }
+    }
+
+    public class MockData
+    {
+        public string CaseNumber { get; set; }
+        public string Name { get; set; }
+        public string TreatmentCenter { get; set; }
+        public IEnumerable<Question> Questions { get; set; }
+    }
+
+    public class Question
+    {
+        public string QuestionText { get; set; }
+        public string Answer { get; set; }
     }
 }
